f3717631a23b38c4278649501209c11faaa6523f
Saina Firooz <saina.firooz@gmail.com>
2024-11-04 21:43:23 +0100

    conditionally render menu items based on current user state

--- src/app/components/navbar/UserMenu.tsx
+++ src/app/components/navbar/UserMenu.tsx
@@ -5,9 +5,17 @@ import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import { User } from "@prisma/client";
+import { signOut } from "next-auth/react";
 
-const UserMenu = () => {
+interface UserMenuProps {
+  currentUser?: User | null;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
   const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
 
   const [isOpen, setIsOpen] = useState(false);
 
@@ -37,8 +45,22 @@ const UserMenu = () => {
       {isOpen && (
         <div className="absolute rounded-xl shadow-lg w-[200px] md:w-[200px] bg-white overflow-hidden right-0 top-12 text-sm">
           <div className="flex flex-col cursor-pointer">
-            <MenuItem onClick={registerModal.onOpen} label="Sign Up" />
-            <MenuItem onClick={() => {}} label="Login" />
+            {currentUser ? (
+              <>
+                <MenuItem onClick={() => {}} label="My trips" />
+                <MenuItem onClick={() => {}} label="My favorites" />
+                <MenuItem onClick={() => {}} label="My reservations" />
+                <MenuItem onClick={() => {}} label="My properties" />
+                <MenuItem onClick={() => {}} label="Airbnb my home" />
+                <hr />
+                <MenuItem onClick={() => signOut()} label="Logout" />
+              </>
+            ) : (
+              <>
+                <MenuItem onClick={registerModal.onOpen} label="Sign Up" />
+                <MenuItem onClick={loginModal.onOpen} label="Login" />
+              </>
+            )}
           </div>
         </div>
       )}
