07010e19ef2ce77a3f69e0836839e6d7c8db3ce1
Saina Firooz <saina.firooz@gmail.com>
2024-11-05 18:45:55 +0100

    added a div for children

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default async function RootLayout({
           <Navbar currentUser={currentUser} />
         </ClientOnly>
 
-        {children}
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
   );
