de21aa56cbf6ebf39aa9f9b5c454b2cee4a91259
Saina Firooz <saina.firooz@gmail.com>
2024-11-05 02:33:09 +0100

    disable reactStrictMode to resolve Leaflet issues in Next.js 15

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  //issues with leaflet in next 15, had to use this
+  reactStrictMode: false,
   /* config options here */
   images: {
     domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
