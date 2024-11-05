cb2ba8f10fc2966bc106da26d580380faddab751
Saina Firooz <saina.firooz@gmail.com>
2024-11-05 16:17:09 +0100

    Added Cloudinary to the allowed image domains in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,11 @@ const nextConfig: NextConfig = {
   reactStrictMode: false,
   /* config options here */
   images: {
-    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+    domains: [
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+      "res.cloudinary.com",
+    ],
   },
 };
 
