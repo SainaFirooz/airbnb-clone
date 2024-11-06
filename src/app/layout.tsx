181ba9fa5dfb97beacdc8b2fc04051500c1d93e7
Saina Firooz <saina.firooz@gmail.com>
2024-11-06 20:21:12 +0100

    added SearchModal component to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import RentModal from "./components/modals/RentModal";
 import LoginModal from "./components/modals/LoginModal";
 import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
+import SearchModal from "./components/modals/searchModal";
 
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
@@ -41,6 +42,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <SearchModal />
           <RentModal />
           <LoginModal />
           <RegisterModal />
